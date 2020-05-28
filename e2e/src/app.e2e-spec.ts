af6a3166a07e76a571f1ade4cc096e81ad29bc36
Shashank <svivek2@slb.com>
2020-05-28 21:11:28 +0530

    updated proj name

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to test-proj!');
+    expect(page.getTitleText()).toEqual('Welcome to user-dashboard-app!');
   });
 
   afterEach(async () => {
